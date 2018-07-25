33fe2d020cdf449c353a48205e2da6ed144a12f9
Goran <gatanaso@users.noreply.github.com>
2018-07-25 17:33:22 +0300

    initial share

---
+++ README.md
@@ -0,0 +1,20 @@
+# \<time-field\>
+
+A simple time field Web Component based on Polymer 3.
+
+### Install the Polymer-CLI
+
+First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) and npm (packaged with [Node.js](https://nodejs.org)) installed.
+Run `npm install` to install the element's dependencies, then run `polymer serve` to serve the element locally.
+
+### Viewing The `<time-field>`
+
+```
+$ polymer serve
+```
+
+### Running Tests
+
+```
+$ wct test --npm -module-resolution=node
+```
---
+++ time-field.js
@@ -0,0 +1,72 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+import './time-field-input.js';
+
+/**
+ * `time-field`
+ * A simple time field input element
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class TimeField extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+
+        [part="value"] {
+          display: flex;
+          align-items: center;
+        }
+
+        [part="separator"] {
+          flex-shrink: 0;
+          text-align: center;
+          font-weight: bold;
+          width: 1em;
+        }
+      </style>
+
+      <div part="value">
+        <time-field-input min="0" max="23" value="{{_hours}}"></time-field-input>
+        <div part="separator">:</div>
+        <time-field-input min="0" max="59" value="{{_minutes}}"></time-field-input>
+      </div>
+    `;
+  }
+  static get properties() {
+    return {
+      _hours: String,
+
+      _minutes: String,
+
+      value: {
+        type: String,
+        observer: '_valueObserver'
+      }
+    };
+  }
+
+  static get observers() {
+    return ['_valueTimeObserver(_hours, _minutes)'];
+  }
+
+  _valueObserver(value) {
+    if (!value) {
+      return;
+    }
+
+    this._hours = value.split(':')[0];
+    this._minutes = value.split(':')[1];
+  }
+
+  _valueTimeObserver(hours, minutes) {
+    this.value = hours && minutes ? `${hours}:${minutes}` : '';
+  }
+}
+
+window.customElements.define('time-field', TimeField);
